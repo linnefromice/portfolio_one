569201cd6340a11171ecd8bca346943e8425da4e
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-02-13 21:35:37 +0900

    create build script for using esbuild

---
+++ build.js
@@ -0,0 +1,15 @@
+const esbuild = require('esbuild')
+const path = require('path')
+
+const options = {
+    define: { 'process.env.NODE_ENV': process.env.NODE_ENV },
+    entryPoints: [path.resolve(__dirname, 'src/index.jsx')],
+    // bundle: true,
+    target: 'es2020',
+    platform: 'browser',
+    outdir: path.resolve(__dirname, 'build'),
+}
+esbuild.build(options).catch(err => {
+    process.stderr.write(err.stderr)
+    process.exit(1)
+})
