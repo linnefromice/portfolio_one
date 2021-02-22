b3058d617e367bfe18a3d40438eb01316e99da2e
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-02-22 11:01:16 +0900

    minor collection

--- src/component/device.tsx
+++ src/component/device.tsx
@@ -20,15 +20,11 @@ const Screen: FC<Props> = ({ children }) => (
   <div className="screen">
     <div
       className="screen_content back_stream_top"
-      style={{
-        backgroundImage: "url('./contents/stream_top.svg')"
-      }}
+      style={{ backgroundImage: "url('./contents/stream_top.svg')" }}
     >
       <div
         className="screen_content back_stream_bottom"
-        style={{
-          backgroundImage: "url('./contents/stream_bottom.svg'})"
-        }}
+        style={{ backgroundImage: "url('./contents/stream_bottom.svg'})" }}
       ></div>
     </div>
     <div className="screen_content">
@@ -40,18 +36,14 @@ const Screen: FC<Props> = ({ children }) => (
 const LeftButton: FC = () => (
   <div
     className="left_button"
-    style={{
-      backgroundImage: "url('./contents/button_left.svg')"
-    }}
+    style={{ backgroundImage: "url('./contents/button_left.svg')" }}
   ></div>
 );
 
 const RightButton: FC = () => (
   <div
     className="right_button"
-    style={{
-      backgroundImage: "url('./contents/button_right.svg')"
-    }}
+    style={{ backgroundImage: "url('./contents/button_right.svg')" }}
   ></div>
 );
 
