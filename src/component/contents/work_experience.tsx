170b09e8e8b3ffed8e3504a6a11a7c5d14d51308
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-04-08 23:53:49 +0900

    modify work_experience - use attribute instead of <br>

--- src/component/contents/work_experience.tsx
+++ src/component/contents/work_experience.tsx
@@ -35,12 +35,12 @@ const WorkExperience: FC = () => {
             primary="Simplex Inc."
             secondary={
               <React.Fragment>
-                <Typography component="span" variant="body1">2016.4 - 2020.4<br/></Typography>
-                <Typography component="span" variant="body2">- Experienced from leading in to O&M / from player to manager.<br/></Typography>
-                <Typography component="span" variant="body2">- Financial(CryptoAssets/FX) trade system for individuals & security company.<br/></Typography>
-                <Typography component="span" variant="body2">- Java(Spring), HTML/CSS, JavaScript(React), AWS, Apache Cordova, Oracle, Tomcat, Github, Jenkins ...<br/></Typography>
-                <Typography component="span" variant="body2">18.4-18.7: Mentor / Training for 150 new graduates<br/></Typography>
-                <Typography component="span" variant="body2">19.7-20.4: Dev lead (having 30 members) / New development of hybrid application & back system in leading<br/></Typography>
+                <Typography display="block" variant="body1">2016.4 - 2020.4</Typography>
+                <Typography display="block" variant="body2">- Experienced from leading in to O&M / from player to manager.</Typography>
+                <Typography display="block" variant="body2">- Financial(CryptoAssets/FX) trade system for individuals & security company.</Typography>
+                <Typography display="block" variant="body2">- Java(Spring), HTML/CSS, JavaScript(React), AWS, Apache Cordova, Oracle, Tomcat, Github, Jenkins ...</Typography>
+                <Typography display="block" variant="body2">18.4-18.7: Mentor / Training for 150 new graduates</Typography>
+                <Typography display="block" variant="body2">19.7-20.4: Dev lead (having 30 members) / New development of hybrid application & back system in leading</Typography>
               </React.Fragment>
             }
           />
@@ -50,10 +50,10 @@ const WorkExperience: FC = () => {
             primary="Money Forward, Inc."
             secondary={
               <React.Fragment>
-                <Typography component="span" variant="body1">2020.6 -<br/></Typography>
-                <Typography component="span" variant="body2">20.6-20.12: Crosscutting product improvement/development for BtoB Services.<br/></Typography>
-                <Typography component="span" variant="body2">20.12-XX.XX: Renewal project for BtoB Services.<br/></Typography>
-                <Typography component="span" variant="body2">- Ruby(Rails), MySQL, Redis, TypeScript, Next.js, Docker, AWS, Kubernetes, Terraform...</Typography>
+                <Typography display="block" variant="body1">2020.6 -</Typography>
+                <Typography display="block" variant="body2">20.6-20.12: Crosscutting product improvement/development for BtoB Services.</Typography>
+                <Typography display="block" variant="body2">20.12-XX.XX: Renewal project for BtoB Services.</Typography>
+                <Typography display="block" variant="body2">- Ruby(Rails), MySQL, Redis, TypeScript, Next.js, Docker, AWS, Kubernetes, Terraform...</Typography>
               </React.Fragment>
             }
           />
@@ -63,9 +63,9 @@ const WorkExperience: FC = () => {
             primary="Alterbo, Inc."
             secondary={
               <React.Fragment>
-                <Typography component="span" variant="body1">2020.12 -<br/></Typography>
-                <Typography component="span" variant="body2">In-house system improvement<br/></Typography>
-                <Typography component="span" variant="body2">- JavaScript, GoogleAppScript, BigQuery, Kintone...</Typography>
+                <Typography display="block" variant="body1">2020.12 -</Typography>
+                <Typography display="block" variant="body2">In-house system improvement</Typography>
+                <Typography display="block" variant="body2">- JavaScript, GoogleAppScript, BigQuery, Kintone...</Typography>
               </React.Fragment>
             }
           />
