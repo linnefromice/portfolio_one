fa3e2df3d3d920a4533a2d3aa393407290cfb452
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-04-18 20:41:33 +0900

    update others products

--- src/component/contents/products/info_other_products.tsx
+++ src/component/contents/products/info_other_products.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Card, CardContent, Chip, Divider, IconButton, Typography } from "@material-ui/core";
 import { FaGithub } from 'react-icons/fa';
+import { GrDeploy } from 'react-icons/gr';
 import { animated, useSpring } from 'react-spring';
 
 const Wrapper: FC = ({ children }) => {
@@ -21,7 +22,7 @@ const Wrapper: FC = ({ children }) => {
   );
 }
 
-const SlideVueCard: FC = () => {
+const AppTweet: FC = () => {
   return (
     <Card
       style={{
@@ -34,41 +35,83 @@ const SlideVueCard: FC = () => {
     >
       <CardContent>
         <Typography variant="h6">
-          slide-vue
+          app_tweet
         </Typography>
         <Typography color="textSecondary">
-          <span>You can make slides with Vue Framework..<br/></span>
-          <span>Core function provided by marp plugin.<br/></span>
+          <span>client/server app like twitter.<br/></span>
+          <span>Use react/rails/docker etc...<br/></span>
         </Typography>
         <Divider/>
         <div style={{ padding: 5 }}>
           <Chip
             style={{ margin: 1 }}
-            label="Vue"
+            label="React"
+            color="secondary"
+            variant="outlined"
+          />
+          <Chip
+            style={{ margin: 1 }}
+            label="Ruby on Rails"
+            color="secondary"
+            variant="outlined"
+          />
+          <Chip
+            style={{ margin: 1 }}
+            label="Docker"
             color="primary"
             variant="outlined"
           />
+        </div>
+        <Divider/>
+        <div style={{ padding: 5 }}>
+          <IconButton
+            style={{ margin: 1 }}
+            href="https://github.com/linnefromice/app_tweet_01"
+          >
+            <FaGithub/>
+          </IconButton>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+const CreditCardForm: FC = () => {
+  return (
+    <Card
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "transparent",
+        borderRadius: "2rem",
+        minHeight: "100%"
+      }}
+    >
+      <CardContent>
+        <Typography variant="h6">
+          credit_card_form
+        </Typography>
+        <Typography color="textSecondary">
+          <span>Credit card form using Nextjs.<br/></span>
+          <span>Idea from medium &apos;Here Are 6 Front-End Challenges to Code&apos;.<br/></span>
+        </Typography>
+        <Divider/>
+        <div style={{ padding: 5 }}>
           <Chip
             style={{ margin: 1 }}
-            label="vuetify"
+            label="Next.js"
             color="secondary"
             variant="outlined"
           />
           <Chip
             style={{ margin: 1 }}
-            label="marp-vue"
+            label="css modules"
             color="secondary"
             variant="outlined"
           />
           <Chip
             style={{ margin: 1 }}
-            label="marp-core"
-            color="secondary"
-            variant="outlined"
-          />
-          <Chip
-            style={{ margin: 1 }}
-            label="vue-awesome-swiper"
+            label="sass"
             color="secondary"
             variant="outlined"
           />
@@ -77,9 +120,15 @@ const SlideVueCard: FC = () => {
         <div style={{ padding: 5 }}>
           <IconButton
             style={{ margin: 1 }}
-            href="https://github.com/linnefromice/slide-vue"
+            href="https://github.com/linnefromice/credit_card_form_nextjs_01"
           >
             <FaGithub/>
+          </IconButton>
+          <IconButton
+            style={{ margin: 1 }}
+            href="https://linnefromice.github.io/credit_card_form_nextjs_01/"
+          >
+            <GrDeploy/>
           </IconButton>
         </div>
       </CardContent>
@@ -90,7 +139,8 @@ const SlideVueCard: FC = () => {
 const InfoOtherProducts: FC = () => {
   return (
     <Wrapper>
-      <SlideVueCard/>
+      <AppTweet/>
+      <CreditCardForm/>
     </Wrapper>
   );
 }
